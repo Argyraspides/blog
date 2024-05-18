2d369d5ce07b6cf2e1b0f85f6a9859e9da7d7aab
Argyraspides <joshivinayak441@gmail.com>
2024-05-19 03:50:58 +1200

    Modified vercel.json for backend

--- server/server.js
+++ server/server.js
@@ -7,9 +7,10 @@ import dailyQuotesRoutes from "../server/routes/dailyQuotes.js"
 const PORT = process.env.PORT || 5050;
 const app = express();
 
-app.use(cors());
 app.use(cors({
-  origin: true
+  origin: 'https://blog-one-bice-12.vercel.app',  
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],  
+  allowedHeaders: ['Content-Type', 'Authorization'], 
 }));
 app.use(express.json());
 
